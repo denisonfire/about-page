9df6aa14ee174a640e91daec367d94428b9103d6
Denis Kuchin <kuchindenis89@gmail.com>
2012-06-11 04:24:39 +0400

    add index.php and config.php

---
+++ config.php
@@ -0,0 +1,16 @@
+$info = array(
+	'firstName'		=> 'Denis',
+	'lastName'		=> 'Smith',
+	'photoURL'		=> '/img/photo.jpg',
+	'birthDay'		=> strtotime('07-08-1989'),
+	'city'			=> 'Penza',
+	'country'		=> 'Russia',
+	'website'		=> 'http://deniskuchin.ru/',
+	'email'			=> 'Kuchindenis89@gmail.com',
+	'cellphone'		=> '89603247350',
+	'description'	=> "Hi! I am a webdeveloper living in ...",
+	'tags'			=> 'Developer, Designer, Photographer',
+	'facebook'		=> 'http://www.facebook.com/deniskuchin',
+	'twitter'		=> 'http://twitter.com/Denisonfire',
+	'github'		=> 'http://github.com/kuchin89'
+);
